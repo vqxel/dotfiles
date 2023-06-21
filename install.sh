bbaff83ec29f6a752b1036ba4519a561e3628377
Reza Bagheri <rezq.bagheri@gmail.com>
2023-06-20 23:20:49 -0700

    Homebrew now auto installs some stuff too.

--- install.sh
+++ install.sh
@@ -1,7 +1,25 @@
 #!/bin/zsh
 
-cat ./.zshrc >> ~/.zshrc
+# Install Homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+  # Install Homebrew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  brew update
+fi
 
+# Install Homebrew formulae needed
+brew install cmake
+brew install gcc
+brew install latexindent
+brew install python
+brew install imagemagick
+brew install nvm
+brew install vim
+
+# Configure ZSH and Vim
+cat ./.zshrc >> ~/.zshrc
 cat ./vimconfig.vim >> ~/.vimrc
 
 vim +PluginInstall +qall
