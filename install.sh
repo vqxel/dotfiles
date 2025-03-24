1c18d04ebd5d2452a8c9985e1f826d488383e5fe
Codehc <57022309+Codehc@users.noreply.github.com>
2025-03-23 22:58:23 -0700

    Fixed some bugs

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ source ${HOME}/dotfiles/installers/$(uname).sh
 
 source ${HOME}/dotfiles/shellConfig/shellConfig.sh
 
+echo "amongus"
+
 # Configure shell and Vim
 ln -sf ${HOME}/dotfiles/shellConfig/shellConfig.sh ${HOME}/.${SHELL_NAME}rc
 ln -sf ${HOME}/dotfiles/vimConfig/vimconfig.vim ${HOME}/.vimrc
