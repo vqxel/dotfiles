17424ad0b3d375e02f3c224b2c49588b1654b57b
Codehc <57022309+Codehc@users.noreply.github.com>
2025-03-23 22:03:54 -0700

    Added tmux config

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ source ${HOME}/dotfiles/shellConfig/shellConfig.sh
 # Configure shell and Vim
 ln -sf ${HOME}/dotfiles/shellConfig/shellConfig.sh ${HOME}/.${SHELL_NAME}rc
 ln -sf ${HOME}/dotfiles/vimConfig/vimconfig.vim ${HOME}/.vimrc
+ln -sf ${HOME}/dotfiles/tmuxConfig/tmux.conf ${HOME}/.tmux.conf
 
 vim -s ${HOME}/dotfiles/vimConfig/vimCommands.com
